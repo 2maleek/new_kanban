23c5a09168d7323e03abb09d3b9976ccf29db451
2maleek <222malikibrahim@gmail.com>
2020-04-07 20:40:39 +0700

    Initial Server

---
+++ server/middlewares/authentication.js
@@ -0,0 +1,15 @@
+const { User } = require('../models')
+const decode = require('../helpers/decode')
+
+module.exports = () => {
+  let decoded = decode(req.headers.access_token)
+  User.findOne({
+    where: { email : decoded.email }
+  })
+  .then(isFound => {
+    isFound ? next() : next({status: 404, message: 'User not found'})
+  })
+  .catch(err => {
+    next(err)
+  })
+}
