acf4e1f9af19b54bfffe2aed901ca0f02ba91cb5
2maleek <222malikibrahim@gmail.com>
2020-04-07 21:05:49 +0700

    add name and organization to users table

--- server/models/user.js
+++ server/models/user.js
@@ -1,9 +1,36 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {});
+  const { hash } = require('../helpers/bcrypt')
+
+  const { Model } = sequelize.Sequelize
+  class User extends Model {}
+  User.init({
+    name: {
+      type: DataTypes.STRING
+    },
+    email: {
+      type: DataTypes.STRING
+    },
+    password: {
+      type: DataTypes.STRING
+    },
+    organization: {
+      type: DataTypes.STRING
+    }
+  }, 
+  { 
+    hooks: {
+      beforeSave(user, options) {
+        return hash(user.password)
+        .then(result => {
+          user.password = result
+          user.organization = 'hacktiv8'
+        })
+      }
+    },
+    sequelize 
+  })
+
   User.associate = function(models) {
     // associations can be defined here
   };
