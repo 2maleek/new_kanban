2ee343893041bd84c34b6c2b40f422fb424dc3ce
2maleek <222malikibrahim@gmail.com>
2020-04-09 04:12:44 +0700

    add try catch for error from jwt

--- server/middlewares/authentication.js
+++ server/middlewares/authentication.js
@@ -2,13 +2,22 @@ const { User } = require('../models')
 const jwt = require('jsonwebtoken')
 
 module.exports = (req, res, next) => {
-  const decoded = jwt.verify(req.headers.access_token, process.env.SECRET)
-  req.user = decoded
+  // Untuk menangkap error dari jwt, jika error dari jwt berarti token tidak valid
+  // mencegah inject sembarang access_token
+  try {
+    const decoded = jwt.verify(req.headers.access_token, process.env.SECRET)
+    req.user = decoded
+  } catch (error) {
+    throw {status: 401, message: 'Not authenticated'}
+  }
   User.findOne({
-    where: { email : decoded.email }
+    where: { email : req.user.email }
   })
   .then(isFound => {
-    isFound ? next() : next({status: 404, message: 'User not found'})
+    if(!isFound){
+      throw {status: 404, message: 'User not found'}
+    }
+    next()
   })
   .catch(err => {
     next(err)
