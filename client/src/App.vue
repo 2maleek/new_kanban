5e026b3c8e866761aa77cb19b1e6d3a7c9883ca3
2maleek <222malikibrahim@gmail.com>
2020-04-09 00:10:32 +0700

    start with vue

---
+++ client/src/App.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  <div class="container">
+     <h1>Hello {{bundler}}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data() {
+    return {
+      bundler: "Parcel"
+    };
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.container {
+  color: green;
+}
+</style>
