a1d2a315f4fe78e93e59147d6d217e00191fb61e
2maleek <222malikibrahim@gmail.com>
2020-04-08 21:31:06 +0700

    remove column organization

--- server/models/task.js
+++ server/models/task.js
@@ -3,11 +3,10 @@ module.exports = (sequelize, DataTypes) => {
   const Task = sequelize.define('Task', {
     title: DataTypes.STRING,
     description: DataTypes.STRING,
-    organization: DataTypes.STRING,
     UserId: DataTypes.INTEGER
   }, {});
   Task.associate = function(models) {
-    // associations can be defined here
+    Task.belongsTo(models.User)
   };
   return Task;
 };
