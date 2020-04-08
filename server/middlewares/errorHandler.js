6f553a9cb4afebb98548e39ef4f2996ae9aee7c4
2maleek <222malikibrahim@gmail.com>
2020-04-08 19:13:04 +0700

    add validation and error handler

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -13,6 +13,8 @@ module.exports = (err, req, res, next) => {
     res.status(403).json({message: err.message})
   }else if(err.status === 404) {
     res.status(404).json({message: err.message})
+  }else if(err.status === 409) {
+    res.status(409).json({message: err.message})
   }else {
     res.status(500).json({message: "Internal Server Error"})
   }
