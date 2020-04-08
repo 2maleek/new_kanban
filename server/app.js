5253e81632d5c4104d576ebef7a9e9f7637c66ff
2maleek <222malikibrahim@gmail.com>
2020-04-08 19:14:49 +0700

    edit path errorHandler

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const routes = require('./routes')
-const errorHandler = require('./helpers/errorHandler')
+const errorHandler = require('./middlewares/errorHandler')
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT
@@ -9,7 +9,7 @@ const port = process.env.PORT
 app.use(express.json())
 app.use(express.urlencoded( { extended: false } ))
 app.use(cors())
-app.use('/', routes)
+app.use(routes)
 app.use(errorHandler)
 
 app.listen(port, () => {
