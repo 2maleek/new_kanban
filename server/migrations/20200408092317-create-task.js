d4adbcd3078025575f9d63ecc66c5c3448d148df
2maleek <222malikibrahim@gmail.com>
2020-04-09 04:11:06 +0700

    add column category

--- server/migrations/20200408092317-create-task.js
+++ server/migrations/20200408092317-create-task.js
@@ -14,6 +14,9 @@ module.exports = {
       description: {
         type: Sequelize.STRING
       },
+      category: {
+        type: Sequelize.STRING,
+      },
       UserId: {
         type: Sequelize.INTEGER
       },
