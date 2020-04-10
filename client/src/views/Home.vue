4ae54c7ebfaf4d93abbbd82e9f53cd9c4d5502ad
2maleek <222malikibrahim@gmail.com>
2020-04-10 14:38:42 +0700

    show data from database to card

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,33 +2,69 @@
   <div id="home" v-if="page === 'home'">
     <div class="ui">
     <div class="board-container">
-      <div class="board">
+      <div class="board ">
         <div class="board-header">Backlog</div>
-        <ul class="card">
-          <li>List card Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore, quia illum alias dolores, velit earum mollitia corporis obcaecati, voluptatibus a sunt expedita tempora vero saepe! Totam asperiores adipisci alias non.</li>
-        </ul>
-        <div class="board-footer">Footer</div>
+        <div class="cards-container bg-danger">
+          <div class="card" v-for="task in backlogTasks">
+            <h5 class="text-center">{{ task.title }}</h5>
+            <p>{{ task.description }}</p>
+            <p><strong style="color: blue;">{{ task.Creator }}</strong></p>
+
+            <p class="btn btn-sm text-right" style="position:absolute; bottom:0; right: 20%;"><a @click="updateTask(task.id)"><img src="https://img.icons8.com/material-sharp/24/000000/edit.png"></a></p>
+
+            <p class="btn btn-sm text-right" style="position:absolute; bottom:0; right: 5%;"><a @click="deleteTask(task.id)" data-toggle="modal" data-target="#exampleModal"><img src="https://img.icons8.com/color/24/000000/delete-forever.png"></a></p>
+            <!-- Ntar ada icon edit delete dan yang bisa act hanya punyanya sendiri-->
+          </div>
+        </div>
+        <div class="board-footer bg-danger text-light">Footer</div>
       </div>
       <div class="board">
         <div class="board-header">Todo</div>
-        <ul class="card">
-          <li>Li LAgi</li>
-        </ul>
-        <div class="board-footer">Footer</div>
+          <div class="card-container bg-warning">
+            <div class="card" v-for="task in todoTasks">
+              <h5 class="text-center">{{ task.title }}</h5>
+              <p>{{ task.description }}</p>
+              <p><strong>{{ task.Creator }}</strong></p>
+              <!-- Ntar ada icon edit delete dan yang bisa act hanya punyanya sendiri-->
+              <p class="btn btn-sm text-right" style="position:absolute; bottom:0; right: 20%;"><a @click="updateTask(task.id)"><img src="https://img.icons8.com/material-sharp/24/000000/edit.png"></a></p>
+
+              <p class="btn btn-sm text-right" style="position:absolute; bottom:0; right: 5%;"><a @click="deleteTask(task.id)" data-toggle="modal" data-target="#exampleModal"><img src="https://img.icons8.com/color/24/000000/delete-forever.png"></a>
+              </p>
+            </div>
+          </div>
+        <div class="board-footer bg-warning text-light">Footer</div>
       </div>
       <div class="board">
         <div class="board-header">Done</div>
-        <ul class="card">
-          <li>Li LAgi</li>
-        </ul>
-        <div class="board-footer">Footer</div>
+        <div class="cards-container bg-info">
+          <div class="card" v-for="task in doneTasks">
+            <h5 class="text-center">{{ task.title }}</h5>
+            <p>{{ task.description }}</p>
+            <p><strong>{{ task.Creator }}</strong></p>
+            <!-- Ntar ada icon edit delete dan yang bisa act hanya punyanya sendiri-->
+            <p class="btn btn-sm text-right" style="position:absolute; bottom:0; right: 20%;"><a @click="updateTask(task.id)"><img src="https://img.icons8.com/material-sharp/24/000000/edit.png"></a></p>
+
+            <p class="btn btn-sm text-right" style="position:absolute; bottom:0; right: 5%;"><a @click="deleteTask(task.id)" data-toggle="modal" data-target="#exampleModal"><img src="https://img.icons8.com/color/24/000000/delete-forever.png"></a></p>
+
+          </div>
+        </div>
+        <div class="board-footer text-light bg-info">Footer</div>
       </div>
       <div class="board">
         <div class="board-header">Completed</div>
-        <ul class="card">
-          <li>Li LAgi</li>
-        </ul>
-        <div class="board-footer">Footer</div>
+        <div class="cards-container bg-success">
+          <div class="card" v-for="task in completedTasks">
+            <h5 class="text-center">{{ task.title }}</h5>
+            <p>{{ task.description }}</p>
+            <p><strong>{{ task.Creator }}</strong></p>
+            <p class="btn btn-sm text-right" style="position:absolute; bottom:0; right: 20%;"><a @click="updateTask(task.id)"><img src="https://img.icons8.com/material-sharp/24/000000/edit.png"></a></p>
+
+            <p class="btn btn-sm text-right" style="position:absolute; bottom:0; right: 5%;"><a @click="deleteTask(task.id)" data-toggle="modal" data-target="#exampleModal"><img src="https://img.icons8.com/color/24/000000/delete-forever.png"></a>
+            </p>
+            <!-- Ntar ada icon edit delete dan yang bisa act hanya punyanya sendiri-->
+          </div>
+        </div>
+        <div class="board-footer text-light bg-success">Footer</div>
       </div>
     </div>
   </div>
@@ -37,7 +73,7 @@
 <script>
 export default {
   name: 'Home',
-  props: [ 'page' ],
+  props: [ 'page', 'backlogTasks', 'todoTasks', 'doneTasks', 'completedTasks' ],
   data() {
     return {
 
@@ -46,5 +82,19 @@ export default {
   methods: {
 
   },
+  watch: {
+    backlogTasks() {
+      console.log('backlog nambah')
+    },
+    todoTasks() {
+      console.log('todo nambah')
+    },
+    doneTasks() {
+      console.log('done nambah')
+    },
+    completedTasks() {
+      console.log('completed nambah')
+    },
+  }
 }
 </script>
