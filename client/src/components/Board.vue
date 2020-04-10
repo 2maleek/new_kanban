18a1b1810c84e8b561b06ddd8abbb8ac438e0935
2maleek <222malikibrahim@gmail.com>
2020-04-10 23:11:54 +0700

    parting board,card

---
+++ client/src/components/Board.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="board ">
+    <div class="board-header">{{ category }}</div>
+    <div :class="classBody">
+      <slot></slot>
+    </div>
+    <div :class="classFooter">{{ boardFooter }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Board',
+  props: [ 'category', 'boardFooter', 'classBody', 'classFooter' ]
+};
+</script>
