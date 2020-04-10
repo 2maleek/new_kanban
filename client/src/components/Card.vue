eec42f3dcb4e0eae8f764253020e69ce3fd24554
2maleek <222malikibrahim@gmail.com>
2020-04-10 19:44:20 +0700

    parting card be components

---
+++ client/src/components/Card.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="card">
+    <h5 class="text-center">{{ title }}</h5>
+    <p>{{ description }}</p>
+    <p><strong style="color: blue;">{{ Creator }}</strong></p>
+
+    <p class="btn btn-sm text-right" style="position:absolute; bottom:0; right: 20%;" data-toggle="modal" data-target="#ModalEdit" @click="getData(title, description, category)">
+      <a><img src="https://img.icons8.com/material-sharp/24/000000/edit.png">
+      </a>
+    </p>
+
+    <p class="btn btn-sm text-right" style="position:absolute; bottom:0; right: 5%;" @click="deleteTask(task.id, 'backlogTasks')">
+      <a>
+        <img src="https://img.icons8.com/color/24/000000/delete-forever.png">
+      </a>
+    </p>
+    <!-- Ntar ada icon edit delete dan yang bisa act hanya punyanya sendiri-->
+  </div>
+</template>
+
+<script>
+export default {
+	name: 'Card',
+	props: [ 'title', 'description', 'Creator' ],
+	data() {
+		return {
+
+		};
+	},
+	methods: {
+		getData() {
+
+		},
+	},
+};
+</script>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -5,48 +5,24 @@
       <div class="board ">
         <div class="board-header">Backlog</div>
         <div class="cards-container bg-danger">
-          <div class="card" v-for="task in backlogTasks">
-            <h5 class="text-center">{{ task.title }}</h5>
-            <p>{{ task.description }}</p>
-            <p><strong style="color: blue;">{{ task.Creator }}</strong></p>
-
-            <p class="btn btn-sm text-right" style="position:absolute; bottom:0; right: 20%;"><a @click="updateTask(task.id)"><img src="https://img.icons8.com/material-sharp/24/000000/edit.png"></a></p>
-
-            <p class="btn btn-sm text-right" style="position:absolute; bottom:0; right: 5%;"><a @click="deleteTask(task.id)" data-toggle="modal" data-target="#exampleModal"><img src="https://img.icons8.com/color/24/000000/delete-forever.png"></a></p>
-            <!-- Ntar ada icon edit delete dan yang bisa act hanya punyanya sendiri-->
-          </div>
+          <Card v-for="task in backlogTasks" :title="task.title" :description="task.description" :Creator="task.Creator">
+          </Card>
         </div>
         <div class="board-footer bg-danger text-light">Footer</div>
       </div>
       <div class="board">
         <div class="board-header">Todo</div>
           <div class="card-container bg-warning">
-            <div class="card" v-for="task in todoTasks">
-              <h5 class="text-center">{{ task.title }}</h5>
-              <p>{{ task.description }}</p>
-              <p><strong>{{ task.Creator }}</strong></p>
-              <!-- Ntar ada icon edit delete dan yang bisa act hanya punyanya sendiri-->
-              <p class="btn btn-sm text-right" style="position:absolute; bottom:0; right: 20%;"><a @click="updateTask(task.id)"><img src="https://img.icons8.com/material-sharp/24/000000/edit.png"></a></p>
-
-              <p class="btn btn-sm text-right" style="position:absolute; bottom:0; right: 5%;"><a @click="deleteTask(task.id)" data-toggle="modal" data-target="#exampleModal"><img src="https://img.icons8.com/color/24/000000/delete-forever.png"></a>
-              </p>
-            </div>
+            <Card v-for="task in todoTasks" :title"task.title" :description="task.description" :Creator="task.Creator">
+            </Card>
           </div>
         <div class="board-footer bg-warning text-light">Footer</div>
       </div>
       <div class="board">
         <div class="board-header">Done</div>
         <div class="cards-container bg-info">
-          <div class="card" v-for="task in doneTasks">
-            <h5 class="text-center">{{ task.title }}</h5>
-            <p>{{ task.description }}</p>
-            <p><strong>{{ task.Creator }}</strong></p>
-            <!-- Ntar ada icon edit delete dan yang bisa act hanya punyanya sendiri-->
-            <p class="btn btn-sm text-right" style="position:absolute; bottom:0; right: 20%;"><a @click="updateTask(task.id)"><img src="https://img.icons8.com/material-sharp/24/000000/edit.png"></a></p>
-
-            <p class="btn btn-sm text-right" style="position:absolute; bottom:0; right: 5%;"><a @click="deleteTask(task.id)" data-toggle="modal" data-target="#exampleModal"><img src="https://img.icons8.com/color/24/000000/delete-forever.png"></a></p>
-
-          </div>
+            <Card v-for="task in doneTasks" :title"task.title" :description="task.description" :Creator="task.Creator">
+            </Card>
         </div>
         <div class="board-footer text-light bg-info">Footer</div>
       </div>
@@ -54,33 +30,82 @@
         <div class="board-header">Completed</div>
         <div class="cards-container bg-success">
           <div class="card" v-for="task in completedTasks">
-            <h5 class="text-center">{{ task.title }}</h5>
-            <p>{{ task.description }}</p>
-            <p><strong>{{ task.Creator }}</strong></p>
-            <p class="btn btn-sm text-right" style="position:absolute; bottom:0; right: 20%;"><a @click="updateTask(task.id)"><img src="https://img.icons8.com/material-sharp/24/000000/edit.png"></a></p>
-
-            <p class="btn btn-sm text-right" style="position:absolute; bottom:0; right: 5%;"><a @click="deleteTask(task.id)" data-toggle="modal" data-target="#exampleModal"><img src="https://img.icons8.com/color/24/000000/delete-forever.png"></a>
-            </p>
-            <!-- Ntar ada icon edit delete dan yang bisa act hanya punyanya sendiri-->
+            <Card v-for="task in completedTasks" :title"task.title" :description="task.description" :Creator="task.Creator">
+            </Card>
           </div>
         </div>
         <div class="board-footer text-light bg-success">Footer</div>
+      </div>
+    </div>
+    <div class="modal fade" id="ModalEdit" tabindex="-1" role="dialog" aria-labelledby="ModalEditLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title text-dark" id="ModalEditLabel">Create Task</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <form>
+              <div class="form-group">
+                <label class="col-form-label text-dark">Title</label>
+                <input type="text" class="form-control "v-model="title">
+              </div>
+              <div class="form-group">
+                <label class="col-form-label text-dark">Category</label>
+                <select class="custom-select" v-model="category">
+                  <option>Backlog</option>
+                  <option>Todo</option>
+                  <option>Done</option>
+                  <option>Complete</option>
+                </select>
+              </div>
+              <div class="form-group">
+                <label class="col-form-label text-dark">Description</label>
+                <textarea class="form-control" v-model="description"></textarea>
+              </div>
+            </form>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary" data-dismiss="modal" @click="updateTask">Create</button>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Card from "../components/Card"
+
 export default {
   name: 'Home',
+  components: {
+    Card,
+  },
   props: [ 'page', 'backlogTasks', 'todoTasks', 'doneTasks', 'completedTasks' ],
   data() {
     return {
-
+      title: null,
+      description: null,
+      category: null,
     };
   },
   methods: {
+    getData(title, description, category) {
+      this.title = title;
+      this.description = description;
+      this.category = category;
+    },
+    updateTask() {
 
+    },
+    deleteTask(id, category) {
+      console.log(id)
+      this.$emit('deleteTask', id, category)
+    }
   },
   watch: {
     backlogTasks() {
